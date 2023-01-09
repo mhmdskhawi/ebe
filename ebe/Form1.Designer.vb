9fb76a72883c1d4f72f0e3a698451f7476f2da1d
MHMD <MHMD@DESKTOP-T58RCO9>
2023-01-09 02:33:34 +0200

    Add project files.

---
+++ ebe/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.VideoSourcePlayer1 = New AForge.Controls.VideoSourcePlayer()
+        Me.ButtonStop = New System.Windows.Forms.Button()
+        Me.ButtonStart = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBoxModes = New System.Windows.Forms.ComboBox()
+        Me.ComboBoxSources = New System.Windows.Forms.ComboBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(713, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'VideoSourcePlayer1
+        '
+        Me.VideoSourcePlayer1.Location = New System.Drawing.Point(424, 72)
+        Me.VideoSourcePlayer1.Name = "VideoSourcePlayer1"
+        Me.VideoSourcePlayer1.Size = New System.Drawing.Size(233, 205)
+        Me.VideoSourcePlayer1.TabIndex = 13
+        Me.VideoSourcePlayer1.Text = "VideoSourcePlayer1"
+        Me.VideoSourcePlayer1.VideoSource = Nothing
+        '
+        'ButtonStop
+        '
+        Me.ButtonStop.Location = New System.Drawing.Point(582, 10)
+        Me.ButtonStop.Name = "ButtonStop"
+        Me.ButtonStop.Size = New System.Drawing.Size(75, 56)
+        Me.ButtonStop.TabIndex = 12
+        Me.ButtonStop.Text = "Stop"
+        Me.ButtonStop.UseVisualStyleBackColor = True
+        '
+        'ButtonStart
+        '
+        Me.ButtonStart.Location = New System.Drawing.Point(501, 10)
+        Me.ButtonStart.Name = "ButtonStart"
+        Me.ButtonStart.Size = New System.Drawing.Size(75, 56)
+        Me.ButtonStart.TabIndex = 11
+        Me.ButtonStart.Text = "Start"
+        Me.ButtonStart.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(150, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Mode:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(143, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Source:"
+        '
+        'ComboBoxModes
+        '
+        Me.ComboBoxModes.FormattingEnabled = True
+        Me.ComboBoxModes.Location = New System.Drawing.Point(193, 45)
+        Me.ComboBoxModes.Name = "ComboBoxModes"
+        Me.ComboBoxModes.Size = New System.Drawing.Size(288, 21)
+        Me.ComboBoxModes.TabIndex = 8
+        '
+        'ComboBoxSources
+        '
+        Me.ComboBoxSources.FormattingEnabled = True
+        Me.ComboBoxSources.Location = New System.Drawing.Point(193, 18)
+        Me.ComboBoxSources.Name = "ComboBoxSources"
+        Me.ComboBoxSources.Size = New System.Drawing.Size(288, 21)
+        Me.ComboBoxSources.TabIndex = 7
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.PictureBox1.Location = New System.Drawing.Point(38, 95)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(280, 263)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 14
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.VideoSourcePlayer1)
+        Me.Controls.Add(Me.ButtonStop)
+        Me.Controls.Add(Me.ButtonStart)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBoxModes)
+        Me.Controls.Add(Me.ComboBoxSources)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents VideoSourcePlayer1 As AForge.Controls.VideoSourcePlayer
+    Friend WithEvents ButtonStop As Button
+    Friend WithEvents ButtonStart As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBoxModes As ComboBox
+    Friend WithEvents ComboBoxSources As ComboBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ ebe/Form1.vb
@@ -0,0 +1,164 @@
+﻿Imports Emgu.CV
+Imports Emgu.CV.CvEnum
+Imports Emgu.CV.Structure
+Imports Emgu.CV.UI
+Imports AForge.Video.DirectShow
+Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        EnumerateVideoDevices()
+    End Sub
+    Sub adsd()
+        On Error Resume Next
+        Dim sSource = "C:\image.jpg"
+        Dim sTarget = "C:\scr\image.jpg"
+        File.Copy(sSource, sTarget, True)
+        Dim face_cascade = New CascadeClassifier("haarcascade_frontalface_default.xml")
+        Dim img = CvInvoke.Imread("C:\scr\image.jpg", LoadImageType.Color)
+
+
+        Dim gray = New Mat()
+        CvInvoke.CvtColor(img, gray, ColorConversion.Bgr2Gray)
+        Dim faces = face_cascade.DetectMultiScale(gray, scaleFactor:=1.1, minNeighbors:=4, minSize:=New Size(30, 30))
+
+        For Each Facsce In faces
+            CvInvoke.Rectangle(img, Facsce, New MCvScalar(255, 0, 0), 2)
+        Next
+
+
+        PictureBox1.Image = img.Bitmap
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        On Error Resume Next
+        Dim sSource = "C:\image.jpg"
+        Dim sTarget = "C:\scr\image.jpg"
+        File.Copy(sSource, sTarget, True)
+
+
+        Dim face_cascade = New CascadeClassifier("haarcascade_frontalface_default.xml")
+        Dim img   = CvInvoke.Imread("C:\scr\image.jpg", LoadImageType.Color)
+
+
+        Dim gray = New Mat()
+        CvInvoke.CvtColor(img, gray, ColorConversion.Bgr2Gray)
+        Dim faces = face_cascade.DetectMultiScale(gray, scaleFactor:=1.1, minNeighbors:=4, minSize:=New Size(30, 30))
+
+        For Each Facsce In faces
+            CvInvoke.Rectangle(img, Facsce, New MCvScalar(255, 0, 0), 2)
+        Next
+
+
+        PictureBox1.Image = img.Bitmap
+
+    End Sub
+
+
+    Private videoDevices As FilterInfoCollection
+    Private videoCapabilities As VideoCapabilities()
+
+    Private videoDevice As VideoCaptureDevice
+
+    Private Sub EnumerateVideoDevices()
+        ' enumerate video devices
+        videoDevices = New FilterInfoCollection(FilterCategory.VideoInputDevice)
+
+        If videoDevices.Count <> 0 Then
+            ' add all devices to combo
+            For Each device As FilterInfo In videoDevices
+                ComboBoxSources.Items.Add(device.Name)
+            Next
+        Else
+            ComboBoxSources.Items.Add("No DirectShow devices found")
+        End If
+
+        ComboBoxSources.SelectedIndex = 0
+    End Sub
+
+    Private Sub EnumerateVideoModes(device As VideoCaptureDevice)
+        ' get resolutions for selected video source
+        Me.Cursor = Cursors.WaitCursor
+
+        ComboBoxModes.Items.Clear()
+
+        Try
+            videoCapabilities = videoDevice.VideoCapabilities
+
+            For Each capabilty As VideoCapabilities In videoCapabilities
+                If Not ComboBoxModes.Items.Contains(capabilty.FrameSize) Then
+                    ComboBoxModes.Items.Add(capabilty.FrameSize)
+                End If
+            Next
+
+            If videoCapabilities.Length = 0 Then
+                ComboBoxModes.Items.Add("Not supported")
+            End If
+
+            ComboBoxModes.SelectedIndex = 0
+        Finally
+            Me.Cursor = Cursors.[Default]
+        End Try
+
+    End Sub
+
+    Private Sub frmCamera_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ComboBoxSources_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxSources.SelectedIndexChanged
+        If videoDevices.Count <> 0 Then
+            videoDevice = New VideoCaptureDevice(videoDevices(ComboBoxSources.SelectedIndex).MonikerString)
+            EnumerateVideoModes(videoDevice)
+        End If
+    End Sub
+
+    Private Sub ButtonStart_Click(sender As Object, e As EventArgs) Handles ButtonStart.Click
+        CameraStart()
+    End Sub
+
+    Private Sub ButtonStop_Click(sender As Object, e As EventArgs) Handles ButtonStop.Click
+        CameraStop()
+    End Sub
+
+    Private Sub CameraStart()
+        If videoDevice IsNot Nothing Then
+            If (videoCapabilities IsNot Nothing) AndAlso (videoCapabilities.Length <> 0) Then
+                videoDevice.DesiredFrameSize = DirectCast(ComboBoxModes.SelectedItem, Size)
+            End If
+
+
+            VideoSourcePlayer1.VideoSource = videoDevice
+            VideoSourcePlayer1.Start()
+        End If
+    End Sub
+
+    Private Sub CameraStop()
+        If VideoSourcePlayer1.VideoSource IsNot Nothing Then
+            ' stop video device
+            VideoSourcePlayer1.SignalToStop()
+            VideoSourcePlayer1.WaitForStop()
+            VideoSourcePlayer1.VideoSource = Nothing
+        End If
+    End Sub
+
+    Private Sub VideoSourcePlayer1_NewFrame(sender As Object, ByRef image As Bitmap) Handles VideoSourcePlayer1.NewFrame
+        On Error Resume Next
+        ' add overlay
+        Dim g As Graphics = Graphics.FromImage(image)
+
+
+        Dim SaveImagePath = "C:\"
+        Dim SaveImageName = "image.jpg"
+        Dim SaveImage As New Bitmap(image)
+
+
+        SaveImage.Save(SaveImagePath + SaveImageName, Imaging.ImageFormat.Jpeg)
+        SaveImage.Dispose()
+
+
+
+        ' g.DrawString("Augmented reality?", New Font("Arial", 16), Brushes.Black, New Rectangle(10, 10, 200, 50))
+        g.Dispose()
+        adsd()
+    End Sub
+End Class
